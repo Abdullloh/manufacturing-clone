118e9094e9e4cd8a1de857a9c6c20198d4bb6ebe
Abdullloh <abdullohjonirgashev@gmail.com>
2024-08-05 23:05:53 +0500

    fix: minor fix

--- src/shared/components/logo/Logo.tsx
+++ src/shared/components/logo/Logo.tsx
@@ -4,7 +4,7 @@ import { FC } from 'react';
 export const Logo: FC = () => {
   return (
     <Flex style={{ margin: '30px' }} align="center" justify="center">
-      <img width={200} height={170} src="/logo.png" alt="" />
+      <img width={130} height={120} src="/logo.png" alt="" />
     </Flex>
   );
 };
