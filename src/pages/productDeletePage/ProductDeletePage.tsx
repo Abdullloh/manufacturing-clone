f0b295bfa71059e8c0300834412eaba6adb5b13f
Abdullloh <abdullohjonirgashev@gmail.com>
2024-08-06 00:18:59 +0500

    fix: minor fix

--- src/pages/productDeletePage/ProductDeletePage.tsx
+++ src/pages/productDeletePage/ProductDeletePage.tsx
@@ -1,16 +1,30 @@
-import { FC, useEffect } from 'react';
+import { Flex, Typography } from 'antd';
+import { FC, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDeleteProductMutation } from '../../features/products/services';
+
+const { Title } = Typography;
 
 export const ProductDeletePage: FC = () => {
   const { id } = useParams();
   const [deleteProduct] = useDeleteProductMutation();
-
+  const [success, setSucced] = useState(false);
   useEffect(() => {
     if (id) {
-      deleteProduct({ id }).then(window.close);
+      deleteProduct({ id }).then(() => setSucced(true));
     }
   }, [id, deleteProduct]);
 
-  return <div>Maxsulot uchirilmoqda</div>;
+  return (
+    <Flex justify="center" align="center">
+      {success ? (
+        <Title style={{ color: 'green' }} level={5}>
+          {' '}
+          Mahsulot muvaffaqiyatli o'chirildi
+        </Title>
+      ) : (
+        <Title level={5}>Mahsulot o'chirilmoqda</Title>
+      )}
+    </Flex>
+  );
 };
